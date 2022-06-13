4e7794083ba27cf622ae5748e9501a9e47f226b6
Chris Salois <csalois@gmail.com>
2022-06-12 22:04:37 -0400

    commit tree f63ace601ecb624631752d91a8a35871dc92d2b1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
+  validate :is_clickbaity
+
+  private 
+
+  def is_clickbaity
+    unless title =~ /Won't Believe|Secret|Top \d+|Guess/
+      errors.add(:title, "Must be clickbaity")
+    end
+  end
 end
